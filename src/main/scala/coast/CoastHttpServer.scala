d706ad7021296f01758807c0c5b62188555af9f9
Rene Weber <weber.rene@live.de>
2015-12-19 16:13:34 +0100

    Implementation for filter and router trait + created config class skeleton + created strucutre for http server class

--- src/main/scala/coast/CoastHttpServer.scala
+++ src/main/scala/coast/CoastHttpServer.scala
@@ -1,8 +1,30 @@
 package coast
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpResponse, HttpRequest}
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Flow
+import coast.config.CoastConfig
+import coast.http.CoastHttpRequest
+import coast.routing.{Filter, Router}
+
 /**
   * Created by reweber on 18/12/2015
   */
-class CoastHttpServer {
+case class CoastHttpServer(router: Router, coastConfig: CoastConfig) {
 
+  implicit val actorSystem = ActorSystem
+  implicit val actorMaterializer = ActorMaterializer
+
+  def run() = {
+    router match {
+      case routerWithFilter: Filter =>
+      case _ =>
+    }
+  }
+
+  private def buildHttpServer() = {
+
+  }
 }
--- src/main/scala/coast/routing/Filter.scala
+++ src/main/scala/coast/routing/Filter.scala
@@ -1,8 +1,10 @@
 package coast.routing
 
+import coast.http.CoastHttpRequest
+
 /**
   * Created by reweber on 18/12/2015
   */
 trait Filter {
-
+  def filter: (CoastHttpRequest => Option[CoastHttpRequest])
 }
