b1908b0c4766489e319a7703b17d5e0cc0533d5a
Rene Weber <weber.rene@live.de>
2015-12-20 01:08:17 +0100

    Basic action impl.

--- src/main/scala/coast/http/Action.scala
+++ src/main/scala/coast/http/Action.scala
@@ -1,8 +1,17 @@
 package coast.http
 
+import akka.stream.scaladsl.Flow
+
 /**
   * Created by reweber on 18/12/2015
   */
-class Action {
+class Action[A](f: (CoastHttpRequest => Flow[CoastHttpRequest, CoastHttpResponse, A])) {
 
+  def run(coastHttpRequest: CoastHttpRequest): Flow[CoastHttpRequest, CoastHttpResponse, A] = {
+    f(coastHttpRequest)
+  }
+}
+
+object Action {
+  def apply(f: CoastHttpRequest => Flow[CoastHttpRequest, CoastHttpResponse, Any]) = new Action(f)
 }
