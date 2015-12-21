5b1ab13b0b4614c8cd18fb7d17a781832c800e0a
Rene Weber <reweber@ebay.com>
2015-12-21 02:33:35 +0100

    Basic server impl. + Change action to handle future and non future response + impl. implicit conversion for coast http response

--- src/main/scala/coast/http/Action.scala
+++ src/main/scala/coast/http/Action.scala
@@ -1,17 +1,24 @@
 package coast.http
 
-import akka.stream.scaladsl.Flow
+import scala.concurrent.Future
 
 /**
   * Created by reweber on 18/12/2015
   */
-class Action[A](f: (CoastHttpRequest => Flow[CoastHttpRequest, CoastHttpResponse, A])) {
+class Action[A](f: Either[(CoastHttpRequest => CoastHttpResponse), (CoastHttpRequest => Future[CoastHttpResponse])]) {
 
-  def run(coastHttpRequest: CoastHttpRequest): Flow[CoastHttpRequest, CoastHttpResponse, A] = {
-    f(coastHttpRequest)
+  def run(coastHttpRequest: CoastHttpRequest): Future[CoastHttpResponse] = {
+    f match {
+      case Left(nonFutureF) => Future(nonFutureF(coastHttpRequest))
+      case Right(futureF) => futureF(coastHttpRequest)
+    }
   }
 }
 
 object Action {
-  def apply(f: CoastHttpRequest => Flow[CoastHttpRequest, CoastHttpResponse, Any]) = new Action(f)
+  def apply(f: CoastHttpRequest => CoastHttpResponse) = new Action(Left(f))
+}
+
+object AsyncAction {
+  def apply(f: CoastHttpRequest => Future[CoastHttpResponse]) = new Action(Right(f))
 }
--- src/main/scala/coast/http/CoastHttpResponse.scala
+++ src/main/scala/coast/http/CoastHttpResponse.scala
@@ -2,7 +2,15 @@ package coast.http
 
 import akka.http.scaladsl.model._
 
+import scala.collection.immutable.Seq
+
 /**
   * Created by reweber on 18/12/2015
   */
 case class CoastHttpResponse(statusCode: StatusCode, httpHeader: Seq[HttpHeader], body: RequestEntity)
+
+object CoastHttpResponse {
+  implicit private[coast] def toAkkaHttpResponse(coastHttpResponse: CoastHttpResponse): HttpResponse = {
+    HttpResponse(coastHttpResponse.statusCode, coastHttpResponse.httpHeader, coastHttpResponse.body)
+  }
+}
