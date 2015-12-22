74286e70ea08e60ccdb7c365214ded9f358af247
Rene Weber <weber.rene@live.de>
2015-12-22 01:18:07 +0100

    Added defaults to config

--- src/main/scala/coast/config/CoastConfig.scala
+++ src/main/scala/coast/config/CoastConfig.scala
@@ -6,8 +6,8 @@ import akka.http.ServerSettings
   * Created by reweber on 19/12/2015
   */
 trait CoastConfig {
-  def interface: Option[String]
-  def port: Option[Int]
-  def asyncParallelism: Option[Int]
-  def serverSettings: Option[ServerSettings]
+  def interface: Option[String] = None
+  def port: Option[Int] = None
+  def asyncParallelism: Option[Int] = None
+  def serverSettings: Option[ServerSettings] = None
 }
