8f29db912db425d90760e1819139eafb3e5f0b1e
Rene Weber <reweber@ebay.com>
2015-12-23 02:42:04 +0100

    Added HttpBody.scala + implicit conversion used in CoastHttpRequest.scala

--- src/main/scala/coast/http/CoastHttpRequest.scala
+++ src/main/scala/coast/http/CoastHttpRequest.scala
@@ -1,14 +1,19 @@
 package coast.http
 
 import akka.http.scaladsl.model._
+import akka.stream.ActorMaterializer
+import coast.http.request.HttpBody
 
 /**
   * Created by reweber on 18/12/2015
   */
-case class CoastHttpRequest(method: HttpMethod, uri: Uri, httpHeader: Seq[HttpHeader], body: RequestEntity)(implicit akkaHttpRequest: HttpRequest)
+case class CoastHttpRequest(method: HttpMethod, uri: Uri, httpHeader: Seq[HttpHeader], body: HttpBody)
+                           (implicit akkaHttpRequest: HttpRequest, private val actorMaterializer: ActorMaterializer)
 
 object CoastHttpRequest {
-  implicit private[coast] def fromAkkaHttpRequest(httpRequest: HttpRequest): CoastHttpRequest = {
-    CoastHttpRequest(httpRequest.method, httpRequest.uri, httpRequest.headers, httpRequest.entity)(httpRequest)
+  import coast.http.request.HttpBody._
+
+  implicit private[coast] def fromAkkaHttpRequest(httpRequest: HttpRequest)(implicit actorMaterializer: ActorMaterializer): CoastHttpRequest = {
+    CoastHttpRequest(httpRequest.method, httpRequest.uri, httpRequest.headers, httpRequest.entity)(httpRequest, actorMaterializer)
   }
 }
