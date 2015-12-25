f2a8e0ddae0b2d7514721c6478bc4282b672766e
Rene Weber <reweber@ebay.com>
2015-12-25 02:40:48 +0100

    Created implicit writer for json

--- src/main/scala/coast/http/response/HttpResponseBodyWriter.scala
+++ src/main/scala/coast/http/response/HttpResponseBodyWriter.scala
@@ -1,8 +1,21 @@
 package coast.http.response
 
+import akka.http.scaladsl.model.ContentTypes
+import akka.util.ByteString
+
 /**
   * Created by reweber on 24/12/2015
   */
 trait HttpResponseBodyWriter[A <: ResponseFormat] {
   def apply(): HttpResponseBody
+}
+
+object HttpResponseBodyWriter {
+  implicit def jsonToHttpResponseBodyWriter(json: io.circe.Json): HttpResponseBodyWriter[Json] = {
+    new HttpResponseBodyWriter[Json] {
+      override def apply(): HttpResponseBody = {
+        HttpResponseBody(ContentTypes.`application/json`, ByteString(json.spaces2))
+      }
+    }
+  }
 }
