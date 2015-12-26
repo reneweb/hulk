fc6db2c8438d883f4aa58480ebabb8d80be201af
Rene Weber <reweber@ebay.com>
2015-12-26 02:08:09 +0100

    Added simple example

---
+++ src/examples/src/coast/simple/Application.scala
@@ -0,0 +1,36 @@
+package coast.simple
+
+import akka.http.scaladsl.model.{HttpMethods, Uri}
+import coast.CoastHttpServer
+import coast.http.{AsyncAction, Ok, Action, RoutingHttpRequest}
+import coast.routing.Router
+import io.circe.Json
+import scala.concurrent.ExecutionContext.Implicits.global
+
+/**
+  * Created by reweber on 26/12/2015
+  */
+object Application extends App {
+
+  val router = new SimpleRouter()
+  CoastHttpServer(router, None).run()
+}
+
+class SimpleRouter() extends Router {
+  val simpleController = new SimpleController()
+
+  override def router: (RoutingHttpRequest) => Action = {
+    case RoutingHttpRequest(HttpMethods.GET, Uri.Path("/test")) => simpleController.testGet
+    case RoutingHttpRequest(HttpMethods.POST, Uri.Path("/test")) => simpleController.testPost
+  }
+}
+
+class SimpleController() {
+  def testGet = Action { request =>
+    Ok(Json.empty)
+  }
+
+  def testPost = AsyncAction { request =>
+    request.body.asJson().map(Ok(_))
+  }
+}
