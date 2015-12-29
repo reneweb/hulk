260772bde0f5bed073813571929ab445332d0660
Rene Weber <reweber@ebay.com>
2015-12-29 00:38:37 +0000

    Extended filtering to have incoming and outgoing filtering capabilities

--- src/main/scala/coast/CoastHttpServer.scala
+++ src/main/scala/coast/CoastHttpServer.scala
@@ -2,17 +2,16 @@ package coast
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.http.scaladsl.model.{HttpResponse, HttpRequest}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Flow
 import coast.config.CoastConfig
-import coast.http.CoastHttpRequest
 import coast.http.CoastHttpRequest._
 import coast.http.CoastHttpResponse._
-import coast.http.RoutingHttpRequest._
+import scala.concurrent.ExecutionContext.Implicits.global
 import coast.routing.{Filters, Router}
 
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 /**
   * Created by reweber on 18/12/2015
@@ -34,29 +33,25 @@ class CoastHttpServer(router: Router, coastConfig: Option[CoastConfig])
 
   private def buildHttpServer(router: Router, filters: Filters) = {
 
-    val filterFunc = reduceFiltersIntoOneFunc(filters)
+    val filtersSeq = filters.filters
 
     val flow: Flow[HttpRequest, HttpResponse, Any] = Flow[HttpRequest]
-      .map(filterFunc(_))
-      .filter(_.isDefined)
-      .map(_.get)
-      .map(request => (request, router.router(request)))
-      .mapAsync(parallelism){ case (request, action) => action.run(request).map(chr => chr) }
+      .mapAsync(parallelism) { request =>
+        val filterResults = filtersSeq.map(_.filter(request))
+        val incomingFilterResponseOpt = filterResults.filter(_.result.isLeft).lastOption.flatMap(_.result.swap.toOption)
+
+        val response = incomingFilterResponseOpt.getOrElse(router.router(request).run(request))
+
+        val outgoingFilterResults = filterResults.filter(_.result.isRight).map(_.result.toOption.get)
+        outgoingFilterResults
+          .foldLeft(response){ case (resp, filter) => resp.flatMap(filter) }
+          .map(toAkkaHttpResponse => toAkkaHttpResponse)
+      }
 
     serverSettingsOpt.map { serverSettings =>
       Http().bindAndHandle(flow, interface, port, serverSettings)
     }.getOrElse {
       Http().bindAndHandle(flow, interface, port)
-    }
-  }
-
-  private def reduceFiltersIntoOneFunc(filters: Filters): (CoastHttpRequest => Option[CoastHttpRequest]) = {
-    if(filters.filters.isEmpty) {
-      req => Some(req)
-    } else {
-      filters.filters.map(_.filter).reduce[CoastHttpRequest => Option[CoastHttpRequest]] {
-        case (filterF, filterOtherF) => filterF andThen (resOpt => resOpt.flatMap(filterOtherF))
-      }
     }
   }
 }
