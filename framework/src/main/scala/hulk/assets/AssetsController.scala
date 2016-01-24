7d581e0f170ac0439016b1182c82ed9d60c6d41a
Rene Weber <reweber@ebay.com>
2016-01-24 01:27:53 +0000

    Created simple assets controller

---
+++ framework/src/main/scala/hulk/assets/AssetsController.scala
@@ -0,0 +1,33 @@
+package hulk.assets
+
+import java.nio.file.Paths
+
+import hulk.http.response.Binary
+import hulk.http.{NotFound, Ok, Action}
+
+import scala.io.{Codec, Source}
+import scala.util.Try
+
+/**
+  * Created by reweber on 23/01/2016
+  */
+class AssetsController {
+
+  def get(path: Option[String] = None) = Action { request =>
+    val assetOpt = request.requestParams.get("file")
+    assetOpt.flatMap { asset =>
+      val sourceOpt =
+        if(asset.startsWith("hulk")) {
+          Try(Source.fromURL(getClass.getResource(Paths.get("/" + asset).toString))).toOption
+        } else {
+          Try(path.map(p => Source.fromFile(Paths.get(p + asset).toString))).toOption.flatten
+        }
+
+      sourceOpt.map { source =>
+        val reader = source.reader()
+        val bytes = Iterator.continually(reader.read).takeWhile(_ != -1).map(_.toByte).toArray
+        Ok[Binary](bytes)
+      }
+    }.getOrElse(NotFound())
+  }
+}
