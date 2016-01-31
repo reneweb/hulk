145f8e5edaec2bc3393d6d2a6b20bad9a87cf378
Rene Weber <reweber@ebay.com>
2016-01-31 23:41:19 +0000

    Moved filtering to own package

---
+++ framework/src/main/scala/hulk/filtering/FilterResult.scala
@@ -0,0 +1,19 @@
+package hulk.filtering
+
+import cats.data.Xor
+import hulk.http.HulkHttpResponse
+
+import scala.concurrent.Future
+
+/**
+  * Created by reweber on 27/12/2015
+  */
+class FilterResult(val result: Xor[Future[HulkHttpResponse], HulkHttpResponse => Future[HulkHttpResponse]])
+
+object FilterResult {
+  implicit def responseToFilterResult(httpResponse: Future[HulkHttpResponse]): FilterResult =
+    new FilterResult(Xor.Left(httpResponse))
+
+  implicit def responseFuncToFilterResult(httpResponseFunc: HulkHttpResponse => Future[HulkHttpResponse]): FilterResult =
+    new FilterResult(Xor.Right(httpResponseFunc))
+}
