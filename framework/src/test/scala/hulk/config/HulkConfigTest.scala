2293094c681f63cb2a600df7469b3d75dde7ff72
Rene Weber <reweber@ebay.com>
2016-02-02 20:14:59 +0000

    Implemented config tests

--- framework/src/test/scala/hulk/config/HulkConfigTest.scala
+++ framework/src/test/scala/hulk/config/HulkConfigTest.scala
@@ -1,6 +1,10 @@
 package hulk.config
 
+import akka.actor.ActorSystem
+import akka.http.ServerSettings
+import com.codahale.metrics.MetricRegistry
 import hulk.config.versioning.{AcceptHeaderVersioning, Versioning}
+import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 
 /**
@@ -25,6 +29,101 @@ class HulkConfigTest extends Specification {
       }
 
       config.versioning must equalTo(None)
+    }
+  }
+
+  "HulkConfig#interface" should {
+    "return some interface if set" >> {
+      val i = Some("localhost")
+
+      val config = new HulkConfig {
+        override def interface: Option[String] = i
+      }
+
+      config.interface must equalTo(i)
+    }
+    "return none if not set" >> {
+      val config = new HulkConfig {
+        override def interface: Option[String] = None
+      }
+
+      config.interface must equalTo(None)
+    }
+  }
+
+  "HulkConfig#port" should {
+    "return some interface if set" >> {
+      val i = Some(1000)
+
+      val config = new HulkConfig {
+        override def port: Option[Int] = i
+      }
+
+      config.port must equalTo(i)
+    }
+    "return none if not set" >> {
+      val config = new HulkConfig {
+        override def port: Option[Int] = None
+      }
+
+      config.port must equalTo(None)
+    }
+  }
+
+  "HulkConfig#asyncParallelism" should {
+    "return some interface if set" >> {
+      val i = Some(5)
+
+      val config = new HulkConfig {
+        override def asyncParallelism: Option[Int] = i
+      }
+
+      config.asyncParallelism must equalTo(i)
+    }
+    "return none if not set" >> {
+      val config = new HulkConfig {
+        override def asyncParallelism: Option[Int] = None
+      }
+
+      config.asyncParallelism must equalTo(None)
+    }
+  }
+
+  "HulkConfig#serverSettings" should {
+    "return some interface if set" >> {
+      val i = Some(ServerSettings(None)(ActorSystem()))
+
+      val config = new HulkConfig {
+        override def serverSettings: Option[ServerSettings] = i
+      }
+
+      config.serverSettings must equalTo(i)
+    }
+    "return none if not set" >> {
+      val config = new HulkConfig {
+        override def serverSettings: Option[ServerSettings] = None
+      }
+
+      config.serverSettings must equalTo(None)
+    }
+  }
+
+  "HulkConfig#metricRegistry" should {
+    "return some metricRegistry if set" >> {
+      val i = Some(new MetricRegistry)
+
+      val config = new HulkConfig {
+        override def metricRegistry: Option[MetricRegistry] = i
+      }
+
+      config.metricRegistry must equalTo(i)
+    }
+    "return none if not set" >> {
+      val config = new HulkConfig {
+        override def metricRegistry: Option[MetricRegistry] = None
+      }
+
+      config.metricRegistry must equalTo(None)
     }
   }
 }
