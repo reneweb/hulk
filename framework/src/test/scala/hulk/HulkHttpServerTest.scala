172ac83866d403bce268203a6dd73bc30c479ff8
Rene Weber <weber.rene@live.de>
2016-02-13 00:51:27 +0000

    Implemented test for HulkHttpServer

--- framework/src/test/scala/hulk/HulkHttpServerTest.scala
+++ framework/src/test/scala/hulk/HulkHttpServerTest.scala
@@ -1,23 +1,37 @@
 package hulk
 
-import akka.http.scaladsl.model.ContentTypes
+import akka.actor.ActorSystem
+import akka.http.scaladsl.model.{HttpMethods, ContentTypes}
+import akka.stream.ActorMaterializer
 import akka.util.ByteString
+import hulk.http.{Ok, Action}
 import hulk.http.response.HttpResponseBody
+import hulk.routing.{RouteDef, Router}
 import org.specs2.mutable.Specification
 
 import scala.concurrent.Await
+import scala.concurrent.duration._
 
 /**
   * Created by reweber on 07/02/2016
   */
 class HulkHttpServerTest extends Specification {
 
-  "HulkHttpServer#toResponseEntity" should {
-    "convert an HttpResponseBody to ResponseEntity" >> {
-      val httpResponseBody = HttpResponseBody(ContentTypes.`text/plain(UTF-8)`, ByteString("SomeString"))
-      val responseEntity = HttpResponseBody.toResponseEntity(httpResponseBody)
+  implicit val actorSystem = ActorSystem()
+  implicit val actorMaterializer = ActorMaterializer()
+  val action = Action(request => Ok())
 
-      responseEntity.contentType must equalTo(ContentTypes.`text/plain(UTF-8)`)
+  "HulkHttpServer#run" should {
+    "create http server" >> {
+      val router = new Router() {
+        override def router: Map[RouteDef, Action] = Map(
+          (HttpMethods.GET, "/route") -> action
+        )
+      }
+
+      val serverBinding = Await.result(HulkHttpServer(router).run(), 5 seconds)
+      serverBinding.localAddress.getHostName must equalTo("localhost")
+      serverBinding.localAddress.getPort must equalTo(10000)
     }
   }
 }
