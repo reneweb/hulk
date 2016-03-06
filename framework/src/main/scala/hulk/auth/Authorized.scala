f015e58550e98ef715c001cc8ca8c995bce7b29c
Rene Weber <weber.rene@live.de>
2016-03-06 14:34:22 +0000

    Implemented authorized filter

---
+++ framework/src/main/scala/hulk/auth/Authorized.scala
@@ -0,0 +1,36 @@
+package hulk.auth
+
+import hulk.http.{Forbidden, HulkHttpRequest, HulkHttpResponse}
+
+import scala.concurrent.Future
+import scalaoauth2.provider._
+
+/**
+  * Created by reweber on 06/03/2016
+  */
+class Authorized[T](dataHandler: ProtectedResourceHandler[T]) {
+
+  def apply(f: HulkHttpRequest => Future[HulkHttpResponse]): HulkHttpRequest => Future[HulkHttpResponse] = {
+    case request =>
+      val accessTokenOpt = findAccessTokenFromQueryString(request).orElse(findAccessTokenFromHeader(request))
+
+      accessTokenOpt.map { accessToken =>
+        val protectedResourceRequest = new ProtectedResourceRequest(Map(), Map("access_token" -> Seq(accessToken)))
+        val authResultFuture = ProtectedResource.handleRequest(protectedResourceRequest, dataHandler)
+        authResultFuture.flatMap { authResult => authResult.fold(
+          err => Future.successful(Forbidden()),
+          success => f(request)
+        )}
+      }.getOrElse(Future.successful(Forbidden()))
+  }
+
+  def andThen(f: HulkHttpRequest => Future[HulkHttpResponse]) = apply(f)
+
+  private def findAccessTokenFromQueryString(request: HulkHttpRequest) = request.queryParams.get("access_token")
+
+  private def findAccessTokenFromHeader(request: HulkHttpRequest) = request.httpHeader
+    .find(_.name() == "Authorization")
+    .map(_.value())
+    .filter(_.startsWith("Bearer"))
+    .map(_.drop(7))
+}
