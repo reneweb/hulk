2612ec306652d97b44fca360944e3f6cef380ce4
Rene Weber <reweber@ebay.com>
2016-03-10 00:02:20 +0000

    Implemented OAuthClientFlow

---
+++ framework/src/main/scala/hulk/auth/OAuthClientFlow.scala
@@ -0,0 +1,21 @@
+package hulk.auth
+
+import akka.http.scaladsl.model.HttpHeader
+
+import scalaoauth2.provider._
+
+/**
+  * Created by reweber on 06/03/2016
+  */
+case class OAuthClientFlow[T](oAuthClientFlowData: OAuthClientFlowData, dataHandler: AuthorizationHandler[T]) {
+
+  val headerMap = Map("Authorization", Seq(oAuthClientFlowData.authorization.value()))
+  val paramMap = Map("grant_type" -> Seq(oAuthClientFlowData.grantType)) ++
+    oAuthClientFlowData.scope.map(s => Map("scope" -> Seq(s))).getOrElse(Map.empty)
+
+  val clientCredentialsRequest = new ClientCredentialsRequest(new AuthorizationRequest(headerMap, paramMap))
+  new ClientCredentials().handleRequest(clientCredentialsRequest, dataHandler)
+
+}
+
+case class OAuthClientFlowData(authorization: HttpHeader, grantType: String, scope: Option[String])
