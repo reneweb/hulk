f88666534b50cd9f12a390f102153ebc9b257b09
Rene Weber <weber.rene@live.de>
2016-03-13 23:28:48 +0000

    Implemented example for OAuthImplicitFlow

---
+++ examples/src/main/scala/hulk/oauth/OAuthImplicitFlowApplication.scala
@@ -0,0 +1,65 @@
+package hulk.oauth
+
+import java.util.Date
+
+import akka.http.scaladsl.model.HttpMethods
+import hulk.HulkHttpServer
+import hulk.auth.{OAuthImplicitFlow, OAuthImplicitFlowData}
+import hulk.http.{BadRequest, Ok, AsyncAction, Action}
+import hulk.routing.{RouteDef, Router}
+import play.api.libs.json.Json
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scalaoauth2.provider.{AccessToken, AuthInfo, AuthorizationRequest, AuthorizationHandler}
+
+/**
+  * Created by reweber on 13/03/2016
+  */
+object OAuthImplicitFlowApplication extends App {
+  val router = new OAuthRouter()
+  HulkHttpServer(router).run()
+}
+
+class OAuthRouter() extends Router {
+  val oAuthImplicitController = new OAuthImplicitController()
+
+  override def router: Map[RouteDef, Action] = Map(
+    (HttpMethods.POST, "/token") -> oAuthImplicitController.token,
+    (HttpMethods.GET, "/restrictedResource") -> oAuthImplicitController.restrictedResource
+  )
+}
+
+class OAuthImplicitController() {
+  def token = AsyncAction { request =>
+    val implicitAuthHandler = new ImplicitAuthorizationHandler()
+    val implicitFlowData = OAuthImplicitFlowData(request.httpHeader.find(_.name() == "Authorization").get, "implicit", None)
+
+    val grantResultFuture = OAuthImplicitFlow(implicitFlowData, implicitAuthHandler).run
+    grantResultFuture.map(grantResult => {
+      Ok(Json.obj("access_token" -> grantResult.accessToken,
+        "refresh_token" -> grantResult.refreshToken,
+        "expires_in" -> grantResult.expiresIn))
+    })
+  }
+
+  def restrictedResource = AsyncAction { request =>
+    println(request.requestParams.mkString(";"))
+    request.body.asJson().map { jsonOpt =>
+      jsonOpt.map(Ok(_)).getOrElse(BadRequest())
+    }
+  }
+}
+
+class ImplicitAuthorizationHandler extends AuthorizationHandler[TestUser] {
+  override def validateClient(request: AuthorizationRequest): Future[Boolean] = ???
+  override def createAccessToken(authInfo: AuthInfo[TestUser]): Future[AccessToken] = ???
+  override def refreshAccessToken(authInfo: AuthInfo[TestUser], refreshToken: String): Future[AccessToken] = ???
+  override def findAuthInfoByRefreshToken(refreshToken: String): Future[Option[AuthInfo[TestUser]]] = ???
+  override def getStoredAccessToken(authInfo: AuthInfo[TestUser]): Future[Option[AccessToken]] = Future(Some(AccessToken("accessToken", None, None, None, new Date())))
+  override def findAuthInfoByCode(code: String): Future[Option[AuthInfo[TestUser]]] = ???
+  override def findUser(request: AuthorizationRequest): Future[Option[TestUser]] = Future.successful(Some(TestUser()))
+  override def deleteAuthCode(code: String): Future[Unit] = ???
+}
+
+case class TestUser()
